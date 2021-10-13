72b28d437fdc13e9d0a4a36011f7314809c77608
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-10-13 20:19:12 +0200

    zmiany w settings aby wyeliminować 500Error

--- books/settings.py
+++ books/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^*ugwztsh*f@r6%^2s57cqqsgr+$gby^x=7rdd$1p8xbl6e(25'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 # dla Heroku
-DEBUG = False
+# DEBUG = False
 
 # ALLOWED_HOSTS = []
 # dla Heroku
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
