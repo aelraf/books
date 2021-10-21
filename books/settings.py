e5c693e54c0b7b2be9843bada4bcdaab45feaa42
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-10-21 20:19:29 +0200

    poprawki pliku settings - szukamy błędu w walidacji stron

--- books/settings.py
+++ books/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 # dla Heroku
-DEBUG = False
+# DEBUG = False
 
 # ALLOWED_HOSTS = []
 # dla Heroku
