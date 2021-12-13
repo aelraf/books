7b0c38ca1f5a8d2601e8cab3aa7ccb8d3a16db1e
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-12-13 23:32:05 +0100

    zmiana requirements oraz sposobu odwoływania się do SECRET_KEY

--- aplikacjaKsiazkowa2/urls.py
+++ aplikacjaKsiazkowa2/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path('delete/<int:id>', views.delete, name='delete'),
     path('gugle', views.gugle, name='gugle'),
     path('my_api/', views.my_api, name='my_api'),
-   # path('api', include(router.urls)),
-   # path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api', include(router.urls)),
+    # path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- books/settings.py
+++ books/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-from dotenv import load_dotenv
+from environ import Env
 
-load_dotenv()
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
