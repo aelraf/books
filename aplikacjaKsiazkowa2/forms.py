fce4c72b45e37ce21a3c32c54eb15016bd910504
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-02 22:58:16 +0100

    migracja + formularz BookForm

---
+++ aplikacjaKsiazkowa2/forms.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.forms import ModelForm
+
+from aplikacjaKsiazkowa2.models import Book
+
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
--- aplikacjaKsiazkowa2/models.py
+++ aplikacjaKsiazkowa2/models.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-import datetime
 
-from django.contrib.auth.models import User
 from django.db import models
-from django.urls import reverse
-from django.utils import timezone
 
 
 class Book(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    pub_date = models.DateField('data publikacji')
+    pub_date = models.DateField('data publikacji', blank=True, null=True)
     isbn = models.CharField(max_length=15, null=True, blank=True)
     pages = models.IntegerField(null=True, blank=True)
     cover = models.CharField(max_length=200, null=True, blank=True)
@@ -19,10 +15,6 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title + " - " + self.author
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     class Meta:
         ordering = ['id']
