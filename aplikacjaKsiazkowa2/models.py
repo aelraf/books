0eb7d99fe1778888b3c1822fc75420ade44cb74a
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-04 19:49:48 +0100

    dodanie tytułów kolumn do bazy (zmiana modelu Book) oraz poprawki HTMLa listy (usunięcie zbędnego kodu) oraz edit.html (dodanie użycia formularza, w wersji pierwszej)

--- aplikacjaKsiazkowa2/models.py
+++ aplikacjaKsiazkowa2/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class Book(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    title = models.CharField('tytuł', max_length=200)
+    author = models.CharField('autor', max_length=200)
     pub_date = models.DateField('data publikacji', blank=True, null=True)
-    isbn = models.CharField(max_length=15, null=True, blank=True)
-    pages = models.IntegerField(null=True, blank=True)
-    cover = models.CharField(max_length=200, null=True, blank=True)
-    language = models.CharField(max_length=5, null=True, blank=True)
+    isbn = models.CharField('ISBN', max_length=15, null=True, blank=True)
+    pages = models.IntegerField('Liczba stron', null=True, blank=True)
+    cover = models.CharField("Link do okładki", max_length=200, null=True, blank=True)
+    language = models.CharField('Język', max_length=5, null=True, blank=True)
 
     def __str__(self):
         return self.title + " - " + self.author
