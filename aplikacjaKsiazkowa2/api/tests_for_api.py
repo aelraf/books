e0967a419c63c816b57db2ea14738a8d30b6b79a
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-08 15:55:01 +0100

    testy do BookDetailApi - dla błędnego (liczbowego) pk oraz dla błędnego (stringowego)pk

--- aplikacjaKsiazkowa2/api/tests_for_api.py
+++ aplikacjaKsiazkowa2/api/tests_for_api.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 import json
 
-from django.db.models import QuerySet
 from django.http import Http404
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework.test import APIRequestFactory, RequestsClient, APIClient
+from rest_framework.test import APIRequestFactory, APIClient
 
-from aplikacjaKsiazkowa2.models import Book
 from aplikacjaKsiazkowa2.tests_for_views import create_brzechwa, create_przechrzta, create_tolkien
 
 
@@ -60,8 +57,17 @@ class BookDetailApiTests(TestCase):
 
     def test_my_api_books_with_bad_pk(self):
         client = APIClient()
-        response = client.get('http://127.0.0.1:8000/api/books/100')
+        with self.assertRaises(ValueError):
+            response = client.get('http://127.0.0.1:8000/api/books/qwerty')
+            self.assertEqual(response.status_code, 200)
 
+    def test_my_api_books_wiht_to_big_pk(self):
+        client = APIClient()
+
+        response = client.get('http://127.0.0.1:8000/api/books/100')
         self.assertEqual(response.status_code, 404)
 
-        self.assertRaises(Http404)
+        print("Response: ... {}".format(response))
+        self.assertContains(response, "404 Not Found")
+
+
