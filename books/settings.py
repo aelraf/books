dd601d9746e99e84465a146eec56aadecce585af
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-09 14:48:38 +0100

    poprawki - ścieżka + staticfiles

--- books/settings.py
+++ books/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'aplikacjaKsiazkowa2/static'), )
 
 
 # Default primary key field type
