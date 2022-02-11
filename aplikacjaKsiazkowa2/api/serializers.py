59c05fde6e6f308f33965bc5000fb13a38b67789
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-11 23:38:20 +0100

    dwa serializery do obsługi API guglowego

--- aplikacjaKsiazkowa2/api/serializers.py
+++ aplikacjaKsiazkowa2/api/serializers.py
@@ -3,11 +3,23 @@
 Klasa serializująca obiekt QuerySet i zwracająca JSONa
 """
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from aplikacjaKsiazkowa2.models import Book
 
 
-class BookSerializer(ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+
+class VolumeInfoSerializer(serializers.Serializer):
+    pages = serializers.IntegerField(required=False, source="pageCount")
+
+
+class GugleSerializer(serializers.Serializer):
+    volumeInfo = VolumeInfoSerializer()
+
+    def to_internal_value(self, data):
+        value = super().to_internal_value(data)
+        return value["volumeInfo"]
