f3051eae9cbd8a9116c13837f2a858b63be95a72
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-11 23:38:59 +0100

    usunięcie niewykorzystanych serializerów

--- aplikacjaKsiazkowa2/api/serializers.py
+++ aplikacjaKsiazkowa2/api/serializers.py
@@ -11,15 +11,3 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-
-
-class VolumeInfoSerializer(serializers.Serializer):
-    pages = serializers.IntegerField(required=False, source="pageCount")
-
-
-class GugleSerializer(serializers.Serializer):
-    volumeInfo = VolumeInfoSerializer()
-
-    def to_internal_value(self, data):
-        value = super().to_internal_value(data)
-        return value["volumeInfo"]
